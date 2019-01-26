ca8b09e51f2105cd637f10b4979fa066fcc6c4eb
Jasper Chan <jasperchan515@gmail.com>
2019-01-26 13:47:16 -0800

    Add react-camera lib

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
+import Camera from 'react-camera';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+    constructor(props) {
+        super(props);
+        this.takePicture = this.takePicture.bind(this);
+    }
+
+    takePicture() {
+        this.camera.capture()
+        .then(blob => {
+            this.img.src = URL.createObjectURL(blob);
+            this.img.onload = () => { URL.revokeObjectURL(this.src); }
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+            </p>
+            <div style={style.container}>
+            <Camera
+            style={style.preview}
+            ref={(cam) => {
+                this.camera = cam;
+            }}
+            >
+            <div style={style.captureContainer} onClick={this.takePicture}>
+            <div style={style.captureButton} />
+            </div>
+            </Camera>
+            </div>
+            <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
+            >
             Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+            </a>
+            </header>
+            </div>
+        );
+    }
 }
+const style = {
+  preview: {
+    position: 'relative',
+  },
+  captureContainer: {
+    display: 'flex',
+    position: 'absolute',
+    justifyContent: 'center',
+    zIndex: 1,
+    bottom: 0,
+    width: '100%'
+  },
+  captureButton: {
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    height: 56,
+    width: 56,
+    color: '#000',
+    margin: 20
+  },
+  captureImage: {
+    width: '100%',
+  }
+};
 
 export default App;
