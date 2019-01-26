2177d01b40173bbe10b5a606f63f40ef6d5738d6
Jasper Chan <jasperchan515@gmail.com>
2019-01-26 14:53:10 -0800

    Switch to react-camera sample, add error handling for no camera

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import Camera from 'react-camera';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+
     constructor(props) {
         super(props);
         this.takePicture = this.takePicture.bind(this);
     }
 
     takePicture() {
-        this.camera.capture()
-        .then(blob => {
-            this.img.src = URL.createObjectURL(blob);
-            this.img.onload = () => { URL.revokeObjectURL(this.src); }
-        })
+        try {
+            this.camera.capture()
+                .then(blob => {
+                    this.img.src = URL.createObjectURL(blob);
+                    this.img.onload = () => { URL.revokeObjectURL(this.src); }
+                })
+        } catch(err) {
+            console.log(err.message);
+        }
+
     }
 
     render() {
         return (
-            <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-            Edit <code>src/App.js</code> and save to reload.
-            </p>
             <div style={style.container}>
             <Camera
             style={style.preview}
             ref={(cam) => {
+                console.log('hello');
                 this.camera = cam;
             }}
             >
@@ -36,43 +37,40 @@ class App extends Component {
             <div style={style.captureButton} />
             </div>
             </Camera>
-            </div>
-            <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-            </a>
-            </header>
+            <img
+            style={style.captureImage}
+            ref={(img) => {
+                this.img = img;
+            }}
+            />
             </div>
         );
     }
 }
-const style = {
-  preview: {
-    position: 'relative',
-  },
-  captureContainer: {
-    display: 'flex',
-    position: 'absolute',
-    justifyContent: 'center',
-    zIndex: 1,
-    bottom: 0,
-    width: '100%'
-  },
-  captureButton: {
-    backgroundColor: '#fff',
-    borderRadius: '50%',
-    height: 56,
-    width: 56,
-    color: '#000',
-    margin: 20
-  },
-  captureImage: {
-    width: '100%',
-  }
-};
 
-export default App;
+const style = {
+    preview: {
+        position: 'relative',
+        border: '1px solid red'
+    },
+    captureContainer: {
+        display: 'flex',
+        position: 'absolute',
+        justifyContent: 'center',
+        zIndex: 1,
+        bottom: 0,
+        width: '100%',
+        border: '1px solid blue'
+    },
+    captureButton: {
+        backgroundColor: '#fff',
+        borderRadius: '50%',
+        height: 56,
+        width: 56,
+        color: '#123',
+        margin: 20
+    },
+    captureImage: {
+        width: '100%',
+    }
+};
